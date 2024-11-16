a02a6902868ab0a60b6ececc30d80bdba75b5375
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-11-16 14:23:55 +0700

    refactor util

--- src/main/java/com/nyanmyohtet/springbootstarter/util/TimeUtil.java
+++ src/main/java/com/nyanmyohtet/springbootstarter/util/TimeUtil.java
@@ -10,6 +10,14 @@ public final class TimeUtil {
         throw new UnsupportedOperationException("Utility class");
     }
 
+    /**
+     * Convert the time value to Duration based on the given unit
+     *
+     * @param time the time value to convert
+     * @param unit the time units (SECONDS, MINUTES, HOURS, ...)
+     * @return the time value in millisecond
+     * @throws IllegalArgumentException if the unit is not supported
+     */
     public static Duration getDuration(int time, String unit) {
         CommonValidationService.validatePositive(time, "Time");
         CommonValidationService.validateString(unit, "Unit");
@@ -22,15 +30,14 @@ public final class TimeUtil {
         };
     }
 
+    /**
+     * Convert time value to millisecond based on given unit
+     * @param time the time value to convert
+     * @param unit the time units (SECONDS, MINUTES, HOURS, ...)
+     * @return the time value in millisecond
+     * @throws IllegalArgumentException if the unit is not supported
+     */
     public static long getMillisecond(int time, String unit) {
-        CommonValidationService.validatePositive(time, "Time");
-        CommonValidationService.validateString(unit, "Unit");
-
-        return switch (unit.toUpperCase()) {
-            case "HOURS" -> Duration.ofHours(time).toMillis();
-            case "MINUTES" -> Duration.ofMinutes(time).toMillis();
-            case "SECONDS" -> Duration.ofSeconds(time).toMillis();
-            default -> throw new IllegalArgumentException("Unsupported time unit: " + unit);
-        };
+        return getDuration(time, unit).toMillis();
     }
 }
