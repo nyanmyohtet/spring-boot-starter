2721548f4c5f298c5d3162e935f4a23b1be983ea
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-11-16 09:18:58 +0700

    feat: Add input validation in constructor

--- src/main/java/com/nyanmyohtet/springbootstarter/service/impl/TokenBucketServiceImpl.java
+++ src/main/java/com/nyanmyohtet/springbootstarter/service/impl/TokenBucketServiceImpl.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class TokenBucketServiceImpl implements TokenBucketService {
@@ -27,6 +29,11 @@ public class TokenBucketServiceImpl implements TokenBucketService {
     public TokenBucketServiceImpl(@Value("${tokenBucket.bandwidth.capacity}") Integer bucketCapacity,
                                   @Value("${tokenBucket.bandwidth.time}") Integer bucketTime,
                                   @Value("${tokenBucket.bandwidth.unit}") String bucketUnit) {
+
+        validatePositive(bucketCapacity, "Bucket capacity must be positive. Given ");
+        validatePositive(bucketTime, "Bucket time must be positive. Given ");
+        validTimeUnit(bucketUnit);
+
         this.bucketCapacity = bucketCapacity;
         this.bucketTime = bucketTime;
         this.bucketUnit = bucketUnit;
@@ -43,5 +50,24 @@ public class TokenBucketServiceImpl implements TokenBucketService {
                         limit.capacity(bucketCapacity)
                                 .refillGreedy(bucketCapacity, period))
                 .build();
+    }
+
+    private void validatePositive(Integer value, String message) {
+        if (value <= 0) throw new IllegalArgumentException(message + value);
+    }
+
+    // TODO: separate into validation service
+    private void validTimeUnit(String value) {
+
+        if (value == null || value.trim().isEmpty()) throw new IllegalArgumentException("Timeunit value is empty.");
+
+        TimeUnit[] timeUnitValues = TimeUnit.values();
+
+        try {
+            TimeUnit.valueOf(value);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Timeunit is not valid. Given " + value + ". Supported values are "
+                    + Arrays.toString(timeUnitValues));
+        }
     }
 }
