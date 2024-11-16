21c5bcac09138fdc886ac78f6e18bafb430bcc94
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-11-16 12:46:47 +0700

    fix: add cleanup mechanism for the cache map

---
+++ src/main/java/com/nyanmyohtet/springbootstarter/config/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package com.nyanmyohtet.springbootstarter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@SuppressWarnings("unused")
+public class SchedulerConfig {
+}
--- src/main/java/com/nyanmyohtet/springbootstarter/util/TimeUtil.java
+++ src/main/java/com/nyanmyohtet/springbootstarter/util/TimeUtil.java
@@ -4,7 +4,12 @@ import com.nyanmyohtet.springbootstarter.service.impl.CommonValidationService;
 
 import java.time.Duration;
 
-public class TimeUtil {
+public final class TimeUtil {
+
+    private TimeUtil() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
     public static Duration getDuration(int time, String unit) {
         CommonValidationService.validatePositive(time, "Time");
         CommonValidationService.validateString(unit, "Unit");
@@ -13,6 +18,18 @@ public class TimeUtil {
             case "HOURS" -> Duration.ofHours(time);
             case "MINUTES" -> Duration.ofMinutes(time);
             case "SECONDS" -> Duration.ofSeconds(time);
+            default -> throw new IllegalArgumentException("Unsupported time unit: " + unit);
+        };
+    }
+
+    public static long getMillisecond(int time, String unit) {
+        CommonValidationService.validatePositive(time, "Time");
+        CommonValidationService.validateString(unit, "Unit");
+
+        return switch (unit.toUpperCase()) {
+            case "HOURS" -> Duration.ofHours(time).toMillis();
+            case "MINUTES" -> Duration.ofMinutes(time).toMillis();
+            case "SECONDS" -> Duration.ofSeconds(time).toMillis();
             default -> throw new IllegalArgumentException("Unsupported time unit: " + unit);
         };
     }
